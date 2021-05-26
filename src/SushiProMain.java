625145c24133830c168718d80e7c8098500be8bf
Aaron Tetens <tetena573@gmail.com>
2021-05-26 17:14:20 -0400

    initial commit

---
+++ src/SushiProMain.java
@@ -0,0 +1,13 @@
+/**
+ * This class is the driver for the SushiPro program. It takes in user input to
+ * continue the game play loop.
+ * 
+ * @author Aaron Tetens
+ */
+public class SushiProMain {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
